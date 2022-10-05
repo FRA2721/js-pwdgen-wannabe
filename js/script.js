d90bc2afcc26b7eebf556b9304c05d95b8471cdb
FRA2721 <cimino.francesco36@gmail.com>
2022-10-05 15:21:57 +0200

    user input section

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 /* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina nomecognomecolorepreferito21*/
 
+// start
+// ..................................................................
+
+// input-data
+const userName = prompt("Hello dear! What is your name?");
+const userSurname = prompt("Great! And what is your surname?");
+const favouriteColor = prompt("Ok dear... Last question! Let me know what is your favourite color, please:");
+
+// output-data
+
+
+// ..................................................................
+// end
